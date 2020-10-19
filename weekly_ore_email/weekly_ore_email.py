13e1ab890fa5f488891328b2eede66620fa443d1
a01023437 <a01023437@itesm.mx>
2020-10-19 15:57:51 -0500

    updated code

--- weekly_ore_email/weekly_ore_email.py
+++ weekly_ore_email/weekly_ore_email.py
@@ -1,27 +1,51 @@
-import json
 import boto3
 import os
-import ast
+from datetime import datetime
 
-destino = 'agimenezzapiola@gmail.com'
+from boto3.dynamodb.conditions import Key
 
+SENDER = os.environ['DefaultSender']
 SES = boto3.client('ses')
+DDB = boto3.resource('dynamodb')
+
+MONTH = int(datetime.today().strftime('%m'))
+DAY = int(datetime.today().strftime('%d'))
+
+
+def check_database():
+    table = DDB.Table('Compleanni')
+    response = table.query(
+        KeyConditionExpression=Key('BDAY_MONTH').eq(MONTH)
+    )
+    print('Printing response from dynamo:\n', response)
+    people = [p for p in response['Items'] if p['BDAY_DAY']==DAY]
+    return people
+
+
+def send_email(data_dict):
+    response = SES.send_email(Source=SENDER,
+                              Destination={'ToAddresses': [data_dict['Email']],
+                                           'CcAddresses': [SENDER]},
+                              Message={'Subject': {
+                                  'Data': data_dict.get('Subject', 'Feliz cumple!!'),
+                                  'Charset': 'utf-8'
+                              },
+                                  'Body': {
+                                      'Text': {
+                                          'Data': data_dict.get('Body', 'Feliz cumple, abrazo grande!! Alfredo')
+                                      }
+                                  }
+                              }
+                              )
+    return response
 
 
 def lambda_handler(event, context):
-    response = SES.send_email(Source='agimenezzapiola@gmail.com',
-    Destination={'ToAddresses': [destino]},
-    Message={'Subject': {
-        'Data': 'Feliz cumple boludo!',
-        'Charset': 'utf-8'
-        },
-        'Body': {
-            'Text': {            'Data': 'Hola siukooo, te mando feliz cumple desde amazon web services\
-            lambdaaaa. Te quiero boludo, programé esto para las 3 GMT. Posta te\
-            quiero siukazo, estoy escribiendo esto con alto entusiasmo para que se\
-            mande. Contame después cuando te llegue :). Por si te da curiosidad\
-            te imprimo el evento de AWS (json): \n %s ' % event }
-        }
-     }
-    )
-    return response
+    print('Printing event:\n', event)
+    people = check_database()
+    if people:
+        print('Sending email(s)\n')
+        return [send_email(p) for p in people]
+    else:
+        print('No birthdays today')
+        return 'Nothing to do today'
